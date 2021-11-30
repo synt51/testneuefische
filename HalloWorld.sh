c0087cac2bbeebca1f5b4d15a8ed212381667bb8
Heinz Schloemer <heinz.schloemer@gmx.net>
2021-11-30 11:49:40 +0100

    HalloWorld gekürzt

--- HalloWorld.sh
+++ HalloWorld.sh
@@ -1,8 +1,7 @@
 echo Hallo World.
 echo Hallo wer bist du?
 read varname.
-echo Hallo $varname. Und dein Nachname?
-read varsurname.
-echo Hallo $varname $varsurname. Schön, dass du da bist. Woher kommst du eigentlich?
+echo Hallo $varname.
+echo Woher kommst du eigentlich?
 read varcity
 echo Ah cool, $varcity ist eine schöne Stadt.
